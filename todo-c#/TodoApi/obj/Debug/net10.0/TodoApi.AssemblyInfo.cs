3bd5a244a7f83515681e09f4d597a0f95c4f7f6c
rajeshwari <rajeshw001@outlook.com>
2026-04-30 20:04:09 -0500

    change name of todo and save button

--- todo-c#/TodoApi/obj/Debug/net10.0/TodoApi.AssemblyInfo.cs
+++ todo-c#/TodoApi/obj/Debug/net10.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94145673121838698787f8f281a1e71bc3956861")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ac0b0126f48c0a0e7fb61b2d3ce5e826f5d6e22")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
